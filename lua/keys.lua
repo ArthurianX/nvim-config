9bdf69f087e275c86fdf3cb95eff683cb48aafe9
Arthur Kovacs <kovacs.arthur.kraid@gmail.com>
2022-10-01 22:02:09 +0300

    Change BarBar keys

--- lua/keys.lua
+++ lua/keys.lua
@@ -28,26 +28,26 @@ map('n', '<leader>fp', [[:Telescope neoclip <CR>]], {desc = "Telescope neoclip"}
 map('n', '<leader>;;', [[:call CocAction('diagnosticInfo') <CR>]], {desc = "Diagnostic Info"})
 
 -- Move to previous/next
-map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
-map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+map('n', '<C-,>', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<C-.>', '<Cmd>BufferNext<CR>', opts)
 -- Re-order to previous/next
-map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
-map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
+map('n', '<C-<>', '<Cmd>BufferMovePrevious<CR>', opts)
+map('n', '<C->>', '<Cmd>BufferMoveNext<CR>', opts)
 -- Goto buffer in position...
-map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
-map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
-map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
-map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
-map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
-map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
-map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
-map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
-map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
-map('n', '<A-0>', '<Cmd>BufferLast<CR>', opts)
+map('n', '<C-1>', '<Cmd>BufferGoto 1<CR>', opts)
+map('n', '<C-2>', '<Cmd>BufferGoto 2<CR>', opts)
+map('n', '<C-3>', '<Cmd>BufferGoto 3<CR>', opts)
+map('n', '<C-4>', '<Cmd>BufferGoto 4<CR>', opts)
+map('n', '<C-5>', '<Cmd>BufferGoto 5<CR>', opts)
+map('n', '<C-6>', '<Cmd>BufferGoto 6<CR>', opts)
+map('n', '<C-7>', '<Cmd>BufferGoto 7<CR>', opts)
+map('n', '<C-8>', '<Cmd>BufferGoto 8<CR>', opts)
+map('n', '<C-9>', '<Cmd>BufferGoto 9<CR>', opts)
+map('n', '<C-0>', '<Cmd>BufferLast<CR>', opts)
 -- Pin/unpin buffer
-map('n', '<A-p>', '<Cmd>BufferPin<CR>', opts)
+map('n', '<C-p>', '<Cmd>BufferPin<CR>', opts)
 -- Close buffer
-map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
+map('n', '<C-c>', '<Cmd>BufferClose<CR>', opts)
 -- Wipeout buffer
 --                 :BufferWipeout
 -- Close commands
